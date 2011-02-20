dff66230881495c1ded00ac9cc2d75894177a9f1
Andrew <andrew.skotzko@gmail.com>
2011-02-20 10:02:05 -0800

    initial commit on v2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thefakeme::Application.config.secret_token = '35fef7befea8d4cd9e8b1f64985913d7dbc546e9d1bbdb3df53ad16b7bbe334205c5d3c98f64c796e28c088509988d1324f828b954dbb3579049cddf43dc6c66'
