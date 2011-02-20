cbfda9eddcad9d9e30b744ec5d626e3ccb1dd7cd
Andrew <andrew.skotzko@gmail.com>
2011-02-20 10:31:44 -0800

    view tweaks & helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return the logo via a helper, rather than explicit code.
+  # def logo
+  #   logo = image_tag("logo.jpeg", :alt => "Snackpack", :class => "round")
+  # end
+  
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
 end
